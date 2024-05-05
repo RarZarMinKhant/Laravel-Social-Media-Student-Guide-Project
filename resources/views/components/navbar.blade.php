22440ceb3294246dd196a0e3eb5f049f73f6312b
Rar Zar <yarzarminkhant2003@gmail.com>
2024-05-06 03:01:43 +0630

    change tailwind

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,34 +1,55 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-secondary">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="/">Navbar</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Features</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">About</a>
-                </li>
-            </ul>
-        </div>
-        <a href="{{ route('profile.index') }}">{{ auth()->user()?->name }}</a>
-        <a href="{{ route('blog.create') }}">
-            <button>Create Blog</button>
+<nav
+    class="sticky top-0 z-10 block w-full max-w-full px-4 py-2 text-black bg-white border rounded-none shadow-md h-max border-white/80 bg-opacity-80 backdrop-blur-2xl backdrop-saturate-200 lg:px-8 lg:py-4">
+    <div class="flex items-center justify-between text-blue-gray-900">
+        <a href="/"
+            class="mr-4 block cursor-pointer py-1.5 font-sans text-base font-medium leading-relaxed text-inherit antialiased">
+            Social-Media
         </a>
-        <form method="POST" action="{{ route('logout') }}">
-            @csrf
+        <div class="flex items-center gap-4">
+            <div class="hidden mr-4 lg:block">
+                <ul class="flex flex-col gap-2 mt-2 mb-4 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
+                    <li class="block p-1 font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
+                        <a href="#" class="flex items-center">
+                            Home
+                        </a>
+                    </li>
+                    <li class="block p-1 font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
+                        <a href="#" class="flex items-center">
+                            Friends
+                        </a>
+                    </li>
+                    <li class="block p-1 font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
+                        <a href="#" class="flex items-center">
+                            Setting
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div class="flex items-center gap-x-1">
+                @auth
+                    <a class="text-black" href="{{ route('blog.create') }}">
+                        <button>Create Blog</button>
+                    </a>
+                    <a href="{{ route('profile.index') }}"><img
+                            src="https://icon2.cleanpng.com/20180523/wxj/kisspng-businessperson-computer-icons-avatar-clip-art-lattice-5b0508dc2ee812.2252011515270566041921.jpg"
+                            class="w-10 h-10 rounded-full">
+                    </a>
+                    <form method="POST" action="{{ route('logout') }}">
+                        @csrf
 
-            <button type="submit"
-                class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                Log Out
-            </button>
-        </form>
+                        <button type="submit"
+                            class="rounded-lg bg-red-500 py-2 px-4 text-center font-sans text-xs font-bold uppercase text-white">
+                            Log Out
+                        </button>
+                    </form>
+                @else
+                    <button
+                        class="rounded-lg bg-black py-2 px-4 text-center font-sans text-xs font-bold uppercase text-white"
+                        type="button">
+                        <span>Login</span>
+                    </button>
+                @endauth
+            </div>
+        </div>
     </div>
 </nav>
